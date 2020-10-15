f9b0852ad26638f0abc1575a61d35469ee03eb15
Lee Houghton <lee.houghton@connectchildcare.com>
2020-10-15 22:12:47 +0100

    Return undefined if the user cancels

--- packages/capacitor-plugin-video/ios/Plugin/Plugin.swift
+++ packages/capacitor-plugin-video/ios/Plugin/Plugin.swift
@@ -7,44 +7,44 @@ import Capacitor
  */
 @objc(CapacitorVideoPlugin)
 public class CapacitorVideoPlugin: CAPPlugin, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    var call: CAPPluginCall?;
-    let imagePickerController = UIImagePickerController();
+    var call: CAPPluginCall?
+    let imagePickerController = UIImagePickerController()
 
     @objc func selectVideo(_ call: CAPPluginCall) {
         if (self.call != nil) {
-            call.error("Video picker is already in progress");
-            return;
+            call.error("Video picker is already in progress")
+            return
         }
         
-        self.call = call;
+        self.call = call
         
         DispatchQueue.main.async {
             self.imagePickerController.sourceType = .photoLibrary
             self.imagePickerController.delegate = self
             self.imagePickerController.mediaTypes = ["public.movie"]
-            self.imagePickerController.allowsEditing = call.getBool("allowEditing") ?? false;
-            self.bridge.viewController.present(self.imagePickerController, animated: true);
+            self.imagePickerController.allowsEditing = call.getBool("allowEditing") ?? false
+            self.bridge.viewController.present(self.imagePickerController, animated: true)
         }
     }
     
     public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true);
+        picker.dismiss(animated: true)
         
-        call?.error("User canceled");
-        call = nil;
+        call?.success([String: Any]())
+        call = nil
     }
     
     public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        picker.dismiss(animated: true);
+        picker.dismiss(animated: true)
         
-        guard let call = self.call else { return };
-        self.call = nil;
+        guard let call = self.call else { return }
+        self.call = nil
         
         guard let videoURL = info[.mediaURL] as? URL else {
-            call.error("Failed to get video URL");
-            return;
+            call.error("Failed to get video URL")
+            return
         }
         
-        call.success([ "path": videoURL.absoluteString ]);
+        call.success([ "path": videoURL.absoluteString ])
     }
 }
